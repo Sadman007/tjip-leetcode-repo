bd8d54abe9e9b22dd4fda6094177691578338a99
Nahin <45636041+Nahin-CDR@users.noreply.github.com>
2022-03-28 20:01:49 +0600

    Video Explanation Link :
    https://youtu.be/75Jj3LR9YAg

---
+++ week01/Majority Element/169. Majority Element.cpp
@@ -0,0 +1,70 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        
+        
+        // NAZMUL HAQUE NAHIN
+        
+ 
+/******************** first Approach ************************/
+        
+
+//Time Complexity = O(n)
+//Space Complexity = O(n)
+           
+        int mx = -1;
+        int maxTimes;
+        
+        map<int,int>mp;
+        
+        for(int i=0; i<nums.size(); i++)
+        {
+            
+            ++mp[nums[i]];
+            
+            if(mx<=mp[nums[i]])
+            {
+                mx = mp[nums[i]];
+                maxTimes = nums[i];
+            }
+        }
+        
+        
+        
+        return maxTimes;
+        
+        
+       
+/******************** Second Approach ************************/
+
+        
+//Time Complexity = O(n)
+//Space Complexity = O(1) 
+        
+        
+           int appear = 0;
+           int majority = 0;
+        
+           for(int i=0; i<nums.size(); i++)
+           {
+               if(appear == 0)
+               {
+                   majority = nums[i];
+                   appear++;
+               }
+               else if(majority != nums[i])
+               {
+                   appear--;
+               }
+               else
+               {
+                   appear++;
+               }
+           }
+            
+           return majority;
+       
+        
+        
+    }
+};
