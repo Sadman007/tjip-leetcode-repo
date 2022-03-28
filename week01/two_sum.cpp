2265608231d3f9eaf8f60478473ebdef144d3b15
Izazur Rahman Shafin <98324351+imShafin@users.noreply.github.com>
2022-03-28 23:41:03 +0600

    Create two_sum.cpp

---
+++ week01/two_sum.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> exist;
+        int len = nums.size();
+        for(int i = 0; i < len; ++i) 
+            exist[nums[i]] = i;
+        for(int i = 0; i < len; ++i)
+        {
+            if (exist.find(target - nums[i]) != exist.end() && i != exist[target-nums[i]]) 
+                return {i, exist[target-nums[i]]};
+        }
+        return {0, 0};
+    }
+};
