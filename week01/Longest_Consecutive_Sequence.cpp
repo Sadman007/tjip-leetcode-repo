14c1507e3b548a44567cf6940980b44380695c24
Izazur Rahman Shafin <98324351+imShafin@users.noreply.github.com>
2022-04-01 22:15:26 +0600

    Create Longest_Consecutive_Sequence.cpp

---
+++ week01/Longest_Consecutive_Sequence.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        unordered_set<int> long_seq(nums.begin(), nums.end());
+        int longest_seq_len = 0;
+        for(auto num : long_seq)
+        {
+            if(long_seq.count(num - 1) == 0)
+            {
+                int curr = num;
+                while(long_seq.count(curr))
+                {
+                    curr++;
+                }
+                longest_seq_len = max(longest_seq_len, curr - num);
+            }
+        }
+        return longest_seq_len;
+    }
+};
