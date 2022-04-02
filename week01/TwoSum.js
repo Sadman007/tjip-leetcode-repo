56384dc4d5e420b30f0161ff05ffdc9a3c631885
mahadi-nsu <mahadi.hassan@northsouth.edu>
2022-04-02 19:03:05 +0600

    Little Spacing issues fixed

--- week01/TwoSum.js
+++ week01/TwoSum.js
@@ -3,20 +3,15 @@
  * @param {number} target
  * @return {number[]}
  */
- var twoSum = function(nums, target) {
-    
-    let store = {}
-    
-     for (let i=0; i<nums.length ;i++ ){
-        let remaining = target - nums[i];
-       
-       let keys = Object.keys(store);
-        if(keys.includes(remaining.toString())){
-       
-         return [store[remaining] , i]
-           
-        }
-       store[nums[i]] = i;
-
-     } 
+var twoSum = function (nums, target) {
+  let store = {};
+  for (let i = 0; i < nums.length; i++) {
+    let remaining = target - nums[i];
+    let keys = Object.keys(store);
+    if (keys.includes(remaining.toString())) {
+      return [store[remaining], i];
+    }
+    store[nums[i]] = i;
+  }
+  return [];
 };
