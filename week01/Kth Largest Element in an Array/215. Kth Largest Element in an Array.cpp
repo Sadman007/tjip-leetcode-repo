e160a5cc921494fab2bb264533d3f0290f06d1db
Nahin <45636041+Nahin-CDR@users.noreply.github.com>
2022-04-02 14:20:46 +0600

    Update 215. Kth Largest Element in an Array.cpp

--- week01/Kth Largest Element in an Array/215. Kth Largest Element in an Array.cpp
+++ week01/Kth Largest Element in an Array/215. Kth Largest Element in an Array.cpp
@@ -3,12 +3,17 @@ public:
     int findKthLargest(vector<int>& nums, int k) {
         
     /*--- first Approach --*/        
+        
+    // Time O(nlogn)
+    //Space O(1)
     sort(nums.begin(),nums.end(),greater<>());
     return nums[k-1];
         
    /*--- Second Approach --*/   
         
         
+     //Time O(n^2)
+    //Space O(1)
     int max = -1;
     for(int i=1; i<=k; i++)
     {
@@ -30,7 +35,8 @@ public:
       
       
       /*--- Third Approach --*/   
-      
+       // Time O(n)
+    //Space O(n)
       
        priority_queue<int>q;
        for(auto i:nums) q.push(i);
