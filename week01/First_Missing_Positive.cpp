bf370dcae8072f9685a40aeefb6a5103b5f3bdd1
Izazur Rahman Shafin <98324351+imShafin@users.noreply.github.com>
2022-04-05 00:05:59 +0600

    Create First_Missing_Positive.cpp

---
+++ week01/First_Missing_Positive.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        unordered_set<int> bucket(nums.begin(), nums.end());
+        int len = nums.size();
+        for(int i = 1; i <= len + 1; i++)
+        {
+            if(bucket.count(i) == 0)
+            {
+                return i;
+            }
+        }
+        return 0;
+    }
+};
