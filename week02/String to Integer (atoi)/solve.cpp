c63af556005409ce0d05982ffbc2ec424b198925
Nahin <45636041+Nahin-CDR@users.noreply.github.com>
2022-04-11 22:32:22 +0600

    Update solve.cpp

--- week02/String to Integer (atoi)/solve.cpp
+++ week02/String to Integer (atoi)/solve.cpp
@@ -5,18 +5,18 @@ public:
         int i=0;
         int len = s.length();
         long long ans = 0;
-        while(i<len and s[i] == ' ') i++;
+        while(i<len and s[i] == ' ') i++; // প্রথম থেকে যত ফাকা জায়গা আছে সব গুলা স্কিপ করতে থাকবো যথক্ষন না ক্যারেক্টার পাই 
         if(i<len and s[i] == '-') minus = true; 
-        if(i<len and (s[i] == '-' or s[i] == '+')) i++;
-        bool overflow = false;
+        if(i<len and (s[i] == '-' or s[i] == '+')) i++; // এখানে - আর + একসাথে থাকলে আমরা জিরো রিটার্ন করে দিব , তাই শুধু মাত্র একটা কেই কাউন্ট করবো 
+        bool overflow = false;                          // মাইনাস এর পরে আবার মাইনাস কিংবা প্লাস এর পরে আবার প্লাস হলেও জিরো রিটার্ন করতে হবে 
         while(i<len)
         {
             long long value = s[i]-'0';
-            if(value < 0 or value >9) break;
+            if(value < 0 or value >9) break; // এখানে আসকি ভ্যালু ০ আর ৯ এর বাইরে হলে তা ইন্টিজার না , তাই ব্রেক করে দিবো 
             else
             {
                 ans = ans*10 + value;;
-                if(ans > 2147483647)
+                if(ans > 2147483647) // আন্সার যদি আমাদের ৩২ বিট ইন্টিজার লিমিট ক্রস করে তাহলেও ব্রেক করবো , হোক সেটা পজিটিভ বা নেগেটিভ 
                 {
                     overflow = true;
                     break;
@@ -29,6 +29,6 @@ public:
             if(minus) return INT_MIN;
             else return INT_MAX;
         }
-        return minus == true?ans*(-1):ans;
+        return minus == true?ans*(-1):ans; // যদি মাইনাস থেকে থাকে সামনে তাহলে শুধু মাইনাস দিয়ে গুন করে আন্সার রিটার্ন করবো । নইলে শুধু আন্সার ।         
     }
 };
