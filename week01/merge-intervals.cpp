a822590f968c1731c6704eb8207d8c6b24984da5
m.montasir97@gmail.com <m.montasir97@gmail.com>
2022-04-21 16:20:45 +0600

    week 1 solutions.

---
+++ week01/merge-intervals.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+#include<algorithm>
+#include<string>
+
+using namespace std;
+
+#define ll long long
+#define MAX 100000
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    
+    return 0;
+}
