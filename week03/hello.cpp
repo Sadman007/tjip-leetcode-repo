5d0ea5100be17fa988d86b87174ce936183a8538
fifo36 <kamalhussain.ali36@gmail.com>
2022-05-09 22:28:01 +0600

    testing again

---
+++ week03/hello.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fread  ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define nl    '\n'
+#define LL    long long
+#define pii   pair<int, int>
+#define ff    first
+#define ss    second
+#define LIM   500000
+
+
+
+int main(){
+    fread //for faster I/O
+    #ifndef ONLINE_JUDGE
+    freopen("ip.txt", "r", stdin);
+    //freopen("out.txt", "w", stdin);
+    #endif
+
+    int T = 1; //cin>>T;
+    for(int tc = 1; tc <= T; tc++){
+
+
+
+    }
+}
