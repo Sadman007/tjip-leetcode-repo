373815db1a8cd6207a1a98034d1fd7e37a3554f3
theakashsarkar <theakashsarkar7@gmail.com>
2022-05-14 16:40:26 +0600

    longest common prefix

---
+++ week02/longestCommonPrefix.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+string findLCP(string &a, string &b)
+{
+	int idx = 0;
+	while(a[idx] && b[idx] && a[idx] == b[idx])
+	{
+		idx++;
+	}
+	return a.substr(0,idx);
+}
+string longestCommonPrefix(std::vector<string>& strs;)
+{
+	string lcp = strs[0];
+	for(auto str : strs)
+	{
+		lcp = findLCP(lcp, str);
+	}
+	return lcp;
+}
